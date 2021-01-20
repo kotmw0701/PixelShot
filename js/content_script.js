4ed6f0c05bb6763fe2cf90b2bb27ca31926be9f4
kotmw0701 <supurasyu@hotmail.co.jp>
2021-01-20 16:24:24 +0900

    :art: スクリーンショットの実装

--- js/content_script.js
+++ js/content_script.js
@@ -6,13 +6,53 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     let picture = document.createElement("div");
     picture.classList.add("picture");
 
-    image.onload = () => {
+    let body = document.body;
+
+    image.addEventListener('load', () => {
         let canvas = document.createElement("canvas");
-        let scale = Math.min(canvas.width / image.width, canvas.height / image.height)
-        canvas.getContext('2d').drawImage(image, 0, 0, image.width * scale, image.height * scale);
+        canvas.width = image.width;
+        canvas.height = image.height;
+        let imageCanvas = canvas.cloneNode(true);
+        imageCanvas.getContext('2d').drawImage(image, 0, 0, image.width, image.height);
+        let context = canvas.getContext('2d');
+        let startX, startY;
+        
+        let drawRect = () => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.fillRect(0, 0, canvas.width, canvas.height);
+        }
+
+        canvas.addEventListener('mousedown', e => {
+            startX = e.offsetX
+            startY = e.offsetY
+        })
+
+        canvas.addEventListener('mouseup', e => {
+            let width = Math.abs(e.offsetX - startX);
+            let height = Math.abs(e.offsetY - startY);
+            if (width < 5 || height < 5) return;
+            startX = Math.min(startX, e.offsetX);
+            startY = Math.min(startY, e.offsetY);
+            context.clearRect(startX, startY, width, height);
+            context.strokeRect(startX, startY, width, height);
+
+            let trim = document.createElement('canvas');
+            trim.width = width;
+            trim.height = height;
+            trim.getContext('2d').drawImage(image, startX, startY, width, height, 0, 0, width, height);
+
+            document.querySelector('.picture').remove();
+            body.classList.remove('--open');
+        })
+        context.fillStyle = "rgba(0, 0, 0, 0.5)"
+        context.strokeStyle = "white";
+        context.lineWidth = 2;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        picture.appendChild(imageCanvas);
         picture.appendChild(canvas);
-        document.body.appendChild(picture);
-    }
+        body.appendChild(picture);
+        body.classList.add('--open');
+    })
 
     sendResponse("This is Response ")
 })
