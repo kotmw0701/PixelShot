c217cb59557fd0b3ceb7cd687f0c843fc2aaf702
kotmw0701 <supurasyu@hotmail.co.jp>
2021-01-22 15:07:04 +0900

    :art: ドット絵に変換する機能の追加

--- js/main.js
+++ js/main.js
@@ -1,57 +1,35 @@
-window.addEventListener('load', e => {
-    chrome.storage.local.get(['base64'], r => {
+let img = document.createElement('img');
 
-        let image = new Image();
-        image.src = r.base64;
+const px = new pixelit({ from: img, to: document.getElementById('viewer') });
 
-        let download = document.getElementById('download');
-        download.href = r.base64;
 
-        image.addEventListener('load', e => {
-            let canvas = document.getElementById('viewer');
-            canvas.width = image.width;
-            canvas.height = image.height;
-            canvas.getContext('2d').drawImage(image, 0, 0, image.width, image.height);
-        })
+
+
+
+let updateImage = (base64) => {
+    let image = new Image();
+    image.addEventListener('load', () => {
+        px.setFromImgSource(image.src);
+        px.draw().pixelate();
     })
-}, false)
+    image.src = base64;
+}
+
+window.addEventListener('load', e => { chrome.storage.local.get(['base64'], r => updateImage(r.base64)) }, false)
 
 document.getElementById('screenshot').addEventListener('click', e => {
-    chrome.tabs.captureVisibleTab(null, {format:"png"}, (screenshotUrl) => {
-        console.log(screenshotUrl);
-
-        let download = document.getElementById('download');
-        download.download = "download.png";
-        download.href = screenshotUrl;
-
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, {base64: screenshotUrl}, (msg) => console.log(msg));
-        })
-    })
+    chrome.tabs.captureVisibleTab(null, { format: "png" }, screenshotUrl => 
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => chrome.tabs.sendMessage(tabs[0].id, { base64: screenshotUrl }, msg => console.log(msg)))
+    )
 }, false)
 
-document.getElementById('upload').addEventListener('change', (e) => {
+document.getElementById('upload').addEventListener('change', e => {
     let image = e.target.files[0];
     let reader = new FileReader();
-    reader.addEventListener('load', () => {
-        chrome.storage.local.set({base64: reader.result}, () => {})
-    });
+    reader.addEventListener('load', () => chrome.storage.local.set({ base64: reader.result }, () => console.log(`set base64: ${reader.result}`)));
     reader.readAsDataURL(image);
 })
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-    if (namespace == 'local' && changes.base64.newValue) {
-        let link = document.getElementById('download');
-        link.href = changes.base64.newValue;
-
-        let image = new Image();
-        image.src = changes.base64.newValue;
-
-        image.addEventListener('load', e => {
-            let canvas = document.getElementById('viewer');
-            canvas.width = image.width;
-            canvas.height = image.height;
-            canvas.getContext('2d').drawImage(image, 0, 0, image.width, image.height);
-        })
-    }
+    if (namespace == 'local' && changes.base64.newValue) updateImage(changes.base64.newValue);
 })
